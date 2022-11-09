e3cd8c2dca8d9b6e25e98827886c554bd4532b64
Andret <bikszma@gmail.com>
2022-11-10 01:48:19 +0300

    Initial Commit

---
+++ ttt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ttt
+//
+//  Created by Андрей on 10.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
